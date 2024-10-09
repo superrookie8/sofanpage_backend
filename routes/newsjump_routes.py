bcf7fb7fb7c2588511dad2b20c46cbf648161878
superrookie8 <74133113+superrookie8@users.noreply.github.com>
2024-10-09 23:46:54 +0900

    크롤링 코드 수정

--- routes/newsjump_routes.py
+++ routes/newsjump_routes.py
@@ -6,7 +6,6 @@ from datetime import datetime, timedelta
 
 newsjumpball_bp = Blueprint('newsjumpball_bp', __name__)
 
-# 날짜 파싱 함수 개선
 def parse_date(date_string):
     try:
         pattern = r"(\d{4}-\d{2}-\d{2}\s*\d{2}:\d{2}:\d{2})"
@@ -18,7 +17,6 @@ def parse_date(date_string):
     except ValueError:
         return None
 
-# 페이지 수를 동적으로 추출하는 함수
 def get_total_pages(soup):
     pagination = soup.select('.pagination a')
     if pagination:
@@ -34,7 +32,6 @@ def should_crawl(db):
     last_crawl_date = last_crawl['date']
     return datetime.now() - last_crawl_date > timedelta(days=30)
 
-# 크롤링 함수
 @newsjumpball_bp.route('/api/jumpball/search/', strict_slashes=False)
 def search_jumpball():
     query = request.args.get('q')
@@ -45,33 +42,18 @@ def search_jumpball():
     news_jumpball = db['news_jumpball']
 
     if should_crawl(db):
-
         print("Performing monthly crawl...")
-
-    # 2015년부터 현재까지의 기사를 가져오도록 설정
-    start_year = 2015
-    end_year = datetime.now().year
-    total_articles = []
-
-    for year in range(start_year, end_year + 1):
-        print(f"Crawling articles for the year {year}")
-        new_articles = crawl_jumpball(query, year)
-
-        if new_articles:
-            total_articles.extend(new_articles)
-
-    if total_articles:
-        news_jumpball.delete_many({})  # 기존 데이터 삭제
-        news_jumpball.insert_many(total_articles)  # 새 데이터 저장
-
-        db['crawl_info'].update_one(
-            {'name': 'jumpball_last_crawl'},
-            {'$set': {'date': datetime.now()}},
-            upsert=True
-        )
+        total_articles = perform_crawl(query, db)
+        if total_articles:
+            news_jumpball.delete_many({})
+            news_jumpball.insert_many(total_articles)
+            db['crawl_info'].update_one(
+                {'name': 'jumpball_last_crawl'},
+                {'$set': {'date': datetime.now()}},
+                upsert=True
+            )
     else:
         print("Using existing data from the last crawl.")
-    
 
     articles = list(news_jumpball.find({
         '$or': [
@@ -99,22 +81,30 @@ def search_jumpball():
 
     return jsonify(data)
 
-# 크롤링 함수에서 특정 연도에 맞춰 기간 설정 추가
-def crawl_jumpball(query, year):
+def perform_crawl(query, db):
+    start_year = 2015
+    end_year = datetime.now().year
+    total_articles = []
+
+    for year in range(start_year, end_year + 1):
+        print(f"Crawling articles for the year {year}")
+        new_articles = crawl_jumpball(query, year, db)
+        if new_articles:
+            total_articles.extend(new_articles)
+
+    return total_articles
+
+def crawl_jumpball(query, year, db):
     base_url = 'https://jumpball.co.kr/news/search.php'
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
     articles = []
 
-    db = current_app.config['db']
-    news_jumpball = db['news_jumpball']
-
-    # 각 연도에 맞춰 기간을 설정
     params = {
         'q': query,
         'sfld': 'subj',
-        'period': 'custom',  # 사용자 정의 기간
-        'startyear': year,  # 시작 연도 설정
-        'endyear': year,  # 끝 연도 설정
+        'period': 'custom',
+        'startyear': year,
+        'endyear': year,
         'pagenum': 0
     }
 
@@ -135,42 +125,45 @@ def crawl_jumpball(query, year):
             continue
 
         soup = BeautifulSoup(response.content, 'html.parser')
-        page_articles = 0
-        for item in soup.select('#listWrap .listPhoto'):
-            title_tag = item.select_one('dt a')
-            summary_tag = item.select_one('.conts')
-            if title_tag:
-                title = title_tag.text.strip()
-                summary = summary_tag.text.strip() if summary_tag else ""
-                if query in title or query in summary:
-                    print(f"Found title: {title}")
-                    link = 'https://jumpball.co.kr' + title_tag['href']
-
-                    # 기사 페이지에서 작성 시간 파싱
-                    article_response = requests.get(link, headers=headers)
-                    article_soup = BeautifulSoup(article_response.content, 'html.parser')
-                    date_tag = article_soup.select_one('.viewTitle > dl > dd')
-                    date_text = date_tag.text.strip() if date_tag else ""
-                    created_at = parse_date(date_text)
-
-                    image_tag = item.select_one('.img a')
-                    image_url = image_tag['style'].split("url('")[1].split("')")[0] if image_tag else None
-
-                    if news_jumpball.find_one({'link': link}):
-                        print(f"Duplicate article found, skipping: {link}")
-                        continue
-
-                    article = {
-                        'title': title, 
-                        'link': link,
-                        'summary': summary,
-                        'image_url': image_url,
-                        'created_at': created_at
-                    }
-
-                    articles.append(article)
-                    page_articles += 1
+        page_articles = extract_articles(soup, query, db)
         print(f"Articles found on page {page} for year {year}: {page_articles}")
 
+    return articles
+
+def extract_articles(soup, query, db):
+    articles = []
+    news_jumpball = db['news_jumpball']
+    for item in soup.select('#listWrap .listPhoto'):
+        title_tag = item.select_one('dt a')
+        summary_tag = item.select_one('.conts')
+        if title_tag:
+            title = title_tag.text.strip()
+            summary = summary_tag.text.strip() if summary_tag else ""
+            if query in title or query in summary:
+                print(f"Found title: {title}")
+                link = 'https://jumpball.co.kr' + title_tag['href']
+
+                article_response = requests.get(link)
+                article_soup = BeautifulSoup(article_response.content, 'html.parser')
+                date_tag = article_soup.select_one('.viewTitle > dl > dd')
+                date_text = date_tag.text.strip() if date_tag else ""
+                created_at = parse_date(date_text)
+
+                image_tag = item.select_one('.img a')
+                image_url = image_tag['style'].split("url('")[1].split("')")[0] if image_tag else None
+
+                if news_jumpball.find_one({'link': link}):
+                    print(f"Duplicate article found, skipping: {link}")
+                    continue
+
+                article = {
+                    'title': title, 
+                    'link': link,
+                    'summary': summary,
+                    'image_url': image_url,
+                    'created_at': created_at
+                }
+
+                articles.append(article)
     return articles
 
--- routes/newsrookie_routes.py
+++ routes/newsrookie_routes.py
@@ -6,28 +6,22 @@ from datetime import datetime, timedelta
 
 newsrookie_bp = Blueprint('newsrookie_bp', __name__)
 
-# 날짜 파싱 함수
 def parse_date(date_string):
     try:
-        # 날짜와 시간을 모두 추출하는 패턴
-        # "2024.09.07 20:03" 형식만 추출 (앞의 기자 이름, 소속 등은 무시)
         pattern = r"(\d{4}\.\d{2}\.\d{2}\s*\d{2}:\d{2})"
         match = re.search(pattern, date_string)
-        
         if match:
-            # 추출된 문자열을 datetime 객체로 변환
             return datetime.strptime(match.group(1), "%Y.%m.%d %H:%M")
         else:
             return None
     except ValueError:
         return None
 
-# 페이지 수를 동적으로 추출하는 함수
 def get_total_pages(soup):
     pagination = soup.select('.pagination a')
     if pagination:
         last_page = pagination[-1].get('href')
-        total_pages = re.search(r'page=(\d+)', last_page).group(1)  # rookie.co.kr의 페이지네이션 확인
+        total_pages = re.search(r'page=(\d+)', last_page).group(1)
         return int(total_pages)
     return 1
 
@@ -38,34 +32,14 @@ def should_crawl(db):
     last_crawl_date = crawl_info['date']
     return datetime.now() - last_crawl_date > timedelta(days=30)
 
-
-# 크롤링 함수 수정
-def crawl_data(query):
+def crawl_data(query, db):
     base_url = 'https://www.rookie.co.kr/news/articleList.html'
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
     articles = []
 
-    db = current_app.config['db']
     news_rookie = db['news_rookie']
-
-    if should_crawl(db):
-        print("Performing monthly crawl...")
-        new_articles = crawl_data(query)
-        if new_articles:
-            news_rookie.insert_many(new_articles)
-            db['crawl_info'].update_one(
-                {'name': 'rookie_last_crawl'},
-                {'$set': {'date': datetime.now()}},
-                upsert=True
-            )
-    else:
-        print("Using existing data from the last crawl.")
-
-
-    # 기존에 수집된 링크들
     existing_links = set(article['link'] for article in news_rookie.find({}, {'link': 1}))
 
-    # 첫 번째 페이지 크롤링 후, 총 페이지 수를 동적으로 추출
     response = requests.get(base_url, headers=headers, params={'sc_word': query, 'view_type': 'sm', 'page': 1})
     if response.status_code != 200:
         print(f"Failed to retrieve the first page: {response.status_code}")
@@ -87,58 +61,56 @@ def crawl_data(query):
             continue
         
         soup = BeautifulSoup(response.content, 'html.parser')
-        page_articles = 0
-        
-        # #section-list > ul > li 구조의 기사 선택
-        for item in soup.select('#section-list > ul > li'):
-            title_tag = item.select_one('.titles a')
-            summary_tag = item.select_one('.lead a')
-            date_tag = item.select_one('.byline em:last-child')  # 날짜 정보는 .byline의 마지막 <em> 요소
-
-            if title_tag:
-                title = title_tag.text.strip()
-                summary = summary_tag.text.strip() if summary_tag else ""
-                
-              
-                if "원조 머슬녀" in title or "원조 머슬녀" in summary:
-                    print(f"Excluding article with keyword: {title}")
-                    continue  # 해당 기사를 제외하고 다음 기사로 넘어감
-
-                if query in title or query in summary:
-                    link = 'https://www.rookie.co.kr' + title_tag['href']
-                    
-                    if link in existing_links:
-                        print(f"Duplicate article found, skipping: {link}")
-                        continue
-                    
-                    print(f"Found title: {title}")
-
-                    # 기사 페이지에서 작성 시간 파싱
-                    date_text = date_tag.text.strip() if date_tag else ""
-                    created_at = parse_date(date_text)
-
-                    image_tag = item.select_one('.thumb img')
-                    image_url = image_tag['src'] if image_tag else None
-                    
-                    article = {
-                        'title': title, 
-                        'link': link,
-                        'summary': summary,
-                        'image_url': image_url,
-                        'created_at': created_at
-                    }
-                    
-                    articles.append(article)
-                    existing_links.add(link)  # 중복 체크용 링크 추가
-                    page_articles += 1
+        page_articles = extract_articles(soup, query, existing_links, articles)
         print(f"Articles found on page {page} from Rookie: {page_articles}")
 
-    # 크롤링한 기사 데이터 저장
     if articles:
         news_rookie.insert_many(articles)
     
-    return articles  # 반드시 리스트를 반환
+    return articles
 
+def extract_articles(soup, query, existing_links, articles):
+    page_articles = 0
+    for item in soup.select('#section-list > ul > li'):
+        title_tag = item.select_one('.titles a')
+        summary_tag = item.select_one('.lead a')
+        date_tag = item.select_one('.byline em:last-child')
+
+        if title_tag:
+            title = title_tag.text.strip()
+            summary = summary_tag.text.strip() if summary_tag else ""
+            
+            if "원조 머슬녀" in title or "원조 머슬녀" in summary:
+                print(f"Excluding article with keyword: {title}")
+                continue
+
+            if query in title or query in summary:
+                link = 'https://www.rookie.co.kr' + title_tag['href']
+                
+                if link in existing_links:
+                    print(f"Duplicate article found, skipping: {link}")
+                    continue
+                
+                print(f"Found title: {title}")
+
+                date_text = date_tag.text.strip() if date_tag else ""
+                created_at = parse_date(date_text)
+
+                image_tag = item.select_one('.thumb img')
+                image_url = image_tag['src'] if image_tag else None
+                
+                article = {
+                    'title': title, 
+                    'link': link,
+                    'summary': summary,
+                    'image_url': image_url,
+                    'created_at': created_at
+                }
+                
+                articles.append(article)
+                existing_links.add(link)
+                page_articles += 1
+    return page_articles
 
 @newsrookie_bp.route('/api/rookie/search/', strict_slashes=False)
 def search_rookie():
@@ -149,25 +121,23 @@ def search_rookie():
     db = current_app.config['db']
     news_rookie = db['news_rookie']
 
-    # 최신 기사의 작성일 확인
-    last_article = news_rookie.find_one(sort=[("created_at", -1)])
+    if should_crawl(db):
+        print("Performing monthly crawl...")
+        crawl_data(query, db)
+        db['crawl_info'].update_one(
+            {'name': 'rookie_last_crawl'},
+            {'$set': {'date': datetime.now()}},
+            upsert=True
+        )
+    else:
+        print("Using existing data from the last crawl.")
 
-    # 크롤링이 필요할 때만 수행 (마지막 기사의 작성일이 한 달 이상 지났을 경우)
-    if not last_article or last_article['created_at'] < datetime.utcnow() - timedelta(days=30):
-        print("Crawling new articles...")
-        new_articles = crawl_data(query)
-
-        if new_articles:  # 새로운 기사가 있는 경우에만 삽입
-            news_rookie.insert_many(new_articles)
-
-    # 데이터베이스에서 검색 결과 반환
     articles = list(news_rookie.find({
         '$or': [
             {'title': {'$regex': query, '$options': 'i'}}
         ]
     }).sort('created_at', -1))
 
-    # 중복 제거 로직 추가
     seen_links = set()
     unique_articles = []
     for article in articles:
